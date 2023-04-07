9b85408cf39117d743e7376914e577eaaf20aac6
frmauro <frmauro8@gmail.com>
2023-04-07 15:26:14 -0300

    implementaçao do crawler

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using PuppeteerSharp;
+﻿using System.Text.Json;
+using PuppeteerSharp;
+using AngleSharp.Dom;
 using AngleSharp;
+
 
 using var browserFetcher = new BrowserFetcher();
 
@@ -17,4 +20,34 @@ var content = await page.GetContentAsync();
 var context = BrowsingContext.New(Configuration.Default);
 var document = await context.OpenAsync(req => req.Content(content));
 
-//Console.WriteLine(content);
+var countries = document.QuerySelectorAll("*")
+    .Where(e => e.LocalName == "div" && e.ClassName == "col-md-4 country")
+    .ToList();
+
+
+List<Country> parsedCountries = new List<Country>();
+
+
+foreach (var c in countries)
+{
+    var lines = c.Text().Split("\n").Select(s => s.Trim()).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+    parsedCountries.Add(new Country(
+        lines[0].Trim(), 
+        lines[1].Split(':')[1], 
+        int.Parse(lines[2].Split(':')[1]),
+        (int) float.Parse(lines[3].Split(':')[1]))
+    );
+}
+
+var jsonCountries = JsonSerializer.Serialize(parsedCountries);
+
+// foreach (var country in parsedCountries)
+//     Console.WriteLine(country.Name);
+
+Console.WriteLine(jsonCountries);
+Console.ReadLine();
+
+public record Country(string Name, string Capital, int Population, int Area);  
+
+
